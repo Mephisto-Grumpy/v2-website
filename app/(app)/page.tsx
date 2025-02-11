4bc9885489c0b16646bdf1f1078f40533fa29261
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-02-11 16:35:11 +0700

    refactor(home): remove GitHubGraphSection and related components for cleaner layout

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,6 +1,5 @@
 import { fetchEducation } from '@/app/(app)/actions'
 import { EducationSection } from '@/components/section/education-section'
-import { GitHubGraphSection } from '@/components/section/github-graph-section'
 import { HeroSection } from '@/components/section/hero-section'
 
 export default async function Home() {
@@ -9,7 +8,6 @@ export default async function Home() {
   return (
     <main className="z-10 mx-auto flex flex-row flex-wrap items-center justify-center gap-14 space-y-16 text-start">
       <HeroSection />
-      <GitHubGraphSection />
       <EducationSection education={education} />
     </main>
   )
