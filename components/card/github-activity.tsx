f2d59b9b774fe89e585a51e61b26c2195c59b8f6
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-02-11 16:58:58 +0700

    refactor(github-activity): simplify GitHub data retrieval and improve layout styling

--- components/card/github-activity.tsx
+++ components/card/github-activity.tsx
@@ -3,9 +3,7 @@ import { redis } from '@/lib/redis'
 import ActivityCalendar from 'rsc-activity-calendar'
 
 export const GitHubActivity = async () => {
-  const github: GitHubProperties = (await redis.get(
-    'github'
-  )) as GitHubProperties
+  const github: GitHubProperties | null = await redis.get('github')
 
   if (!github) return null
 
@@ -19,7 +17,7 @@ export const GitHubActivity = async () => {
   const fourthQuarterData = github.data.slice(quarterLength * 3)
 
   return (
-    <div className="relative grid gap-0.5 sm:grid-cols-2 sm:gap-[3px] sm:p-8 lg:gap-1">
+    <div className="relative grid grid-cols-2 gap-0.5 sm:grid-cols-2 sm:gap-[3px] sm:p-8 lg:gap-1">
       {[
         thirdQuarterData,
         fourthQuarterData,
@@ -32,14 +30,13 @@ export const GitHubActivity = async () => {
           hideTotalCount
           hideMonthLabels
           data={data}
-          // weekStart={1}
           theme={{
-            light: ['#f7f7f7', '#d1fae5', '#10b981', '#047857', '#065f46'],
-            dark: ['#1f2937', '#d1fae5', '#10b981', '#047857', '#065f46']
+            light: ['#ebedf0', '#9be9a8', '#40c463', '#30a14e', '#216e39'],
+            dark: ['#161b22', '#0e4429', '#006d32', '#26a641', '#39d353']
           }}
         />
       ))}
-      <div className="to-backdrop absolute bottom-0 left-0 right-0 z-10 h-full bg-gradient-to-b from-transparent sm:bottom-8 sm:h-40" />
+      <div className="absolute bottom-0 left-0 right-0 z-10 h-full bg-gradient-to-b from-transparent to-background sm:bottom-8 sm:h-40" />
       <a
         className="absolute bottom-4 left-1/2 z-10 -translate-x-1/2 whitespace-nowrap font-mono text-xs text-muted-foreground"
         href="https://github.com/PunGrumpy"
